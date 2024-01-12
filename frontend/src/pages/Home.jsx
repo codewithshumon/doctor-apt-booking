e5b3fa12711b14d815a3d3ab0d7aa86565693459
Shumon Khan <backlink.shumonkhan@gmail.com>
2024-01-13 00:41:26 +0600

    Complete about section in landing page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import heroImage03 from './../assets/images/hero-img03.png';
 import icon01 from './../assets/images/icon01.png';
 import icon02 from './../assets/images/icon02.png';
 import icon03 from './../assets/images/icon03.png';
+import About from '../components/about/About';
 
 const Home = () => {
   return (
@@ -162,6 +163,8 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+      <About />
     </>
   );
 };
