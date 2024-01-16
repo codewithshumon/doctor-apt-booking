6a9596574cb63d318601ba316bd43afcbd7e87ab
Shumon Khan <backlink.shumonkhan@gmail.com>
2024-01-16 21:22:53 +0600

    Complete the footer sectiong for landing page and added bg png

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { RiLinkedinFill } from 'react-icons/ri';
 import {
   AiFillYoutube,
@@ -73,27 +74,93 @@ const quickLink03 = [
     display: 'Contact Us',
   },
 ];
-const quickLink02 = [
-  {
-    path: '/fing-a-doctor',
-    display: 'Find a Doctor',
-  },
-  {
-    path: '/appointment',
-    display: 'Request a Appointment',
-  },
-  {
-    path: '/location',
-    display: 'Find a Location',
-  },
-  {
-    path: '/opinion',
-    display: 'Get a Opinion',
-  },
-];
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="footer_section pb-16 pt-10">
+      <div className="container">
+        <div className="flex justify-between flex-col md:flex-row flex-wrap gap-[30px]">
+          <div>
+            <img src={logo} alt="logo" />
+            <p className="text-[16px] leading-7 font-[400] text-textColor mt-4">
+              Copyright © {year} developed by SoftoZayaan all right reserved.
+            </p>
+
+            <div className="flex items-center gap-3 mt-4">
+              {socialLinks.map((link, index) => (
+                <Link
+                  to={link.path}
+                  key={index}
+                  className="w-9 h-9 border border-solid border-[#181A1E] rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  {link.icon}
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              Quick Links
+            </h2>
+
+            <ul>
+              {quickLink01.map((item, index) => (
+                <li key={index} className="mb-4">
+                  <Link
+                    to={item.path}
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {item.display}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              I want to:
+            </h2>
+
+            <ul>
+              {quickLink02.map((item, index) => (
+                <li key={index} className="mb-4">
+                  <Link
+                    to={item.path}
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {item.display}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              Support
+            </h2>
+
+            <ul>
+              {quickLink03.map((item, index) => (
+                <li key={index} className="mb-4">
+                  <Link
+                    to={item.path}
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {item.display}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
