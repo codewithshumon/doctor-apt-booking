b17c43f941618e0bd90bd3b976caa75530897054
Shumon Khan <backlink.shumonkhan@gmail.com>
2024-01-21 18:21:48 +0600

    added  login api functionalities

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,9 +1,18 @@
 /* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
 import User from './../models/UserSchema.js';
 import Doctor from './../models/DoctorSchema.js';
+
+const generateToken = (user) => {
+  return jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: '7d' }
+  );
+};
 
 export const register = async (req, res) => {
   const { name, email, password, role, photo, gender } = req.body;
@@ -22,7 +31,6 @@ export const register = async (req, res) => {
 
     //checking if the user exist
     if (user) {
-      console.log('find out the user from database', user);
       return res.status(400).json({ message: 'User already exist' });
     }
 
@@ -64,9 +72,55 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  //can't use password direcly to compare. if do then err: can't access befoer insitializ
+  const userPassword = password;
+
   try {
-    console.log('loging api');
+    let user = null;
+
+    const patient = await User.findOne({ email });
+    const doctor = await Doctor.findOne({ email });
+
+    if (patient) {
+      console.log('patient', patient);
+      user = patient;
+    }
+    if (doctor) {
+      console.log('doctor', doctor);
+      user = doctor;
+    }
+
+    //checking if user exist or not
+    if (!user) {
+      console.log('user', user);
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    //compare password with database haspassword
+    const isPasswordMatch = await bcrypt.compare(userPassword, user.password);
+
+    if (!isPasswordMatch) {
+      return res
+        .status(400)
+        .json({ status: false, message: 'Invalid creadentials' });
+    }
+
+    //get token
+    const token = generateToken(user);
+
+    const { password, role, appointments, ...rest } = user._doc;
+
+    res.status(200).json({
+      status: true,
+      message: 'Successfully logged in',
+      token,
+      data: { ...rest },
+      role,
+    });
   } catch (error) {
-    console.log('loging api');
+    console.log('error', error);
+    res.status(500).json({ status: false, message: 'Failed to login' });
   }
 };
