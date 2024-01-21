780f1fd23ab24792397a32ed3dade0c8e90aac43
Shumon Khan <backlink.shumonkhan@gmail.com>
2024-01-21 13:51:00 +0600

    setup backend with mongodb cloud

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+const corsOptions = {
+  origin: true,
+};
+
+mongoose.set('strictQuery', false);
+const connnectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+
+    console.log('MongoDB databased is connected');
+  } catch (error) {
+    console.log(`MongoDB databased connection failed: ${error}`);
+  }
+};
+
+app.listen(port, () => {
+  connnectDB();
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('API is working');
+});
+
+//middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
