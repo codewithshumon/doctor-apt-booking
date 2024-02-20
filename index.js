f3950ee68ad3cc476fea40509ef801e0276951bb
Shumon Khan <backlink.shumonkhan@gmail.com>
2024-02-20 16:54:11 +0600

    fixing api error

--- index.js
+++ index.js
@@ -28,12 +28,14 @@ app.use(
 // Problem is when after deploying the app on vercel or netlify  I try to navigate the route then it's working but when refresh the page then it's shwoing the page not found
 // When you refresh the page on a route other than the root route ("/"), the server doesn't recognize the route because it's expecting a different server-side route. This results in a "page not found" error
 
+const _dirname = path.resolve();
+
 // Serve static files from the build folder
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(_dirname, 'build')));
 
 // Define a wildcard route that serves index.html for all routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(_dirname, 'build', 'index.html'));
 });
 //THIS IS THE PROBLEM
 
