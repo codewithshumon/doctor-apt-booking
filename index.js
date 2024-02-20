35bed52cb23435d72095635104a301a459d3b832
Shumon Khan <backlink.shumonkhan@gmail.com>
2024-02-20 17:07:50 +0600

    Fixing API

--- index.js
+++ index.js
@@ -24,19 +24,19 @@ app.use(
   })
 );
 
-//THIS IS THE PROBLEM
+//THIS IS THE PROBLEM ""THIS IS ONLY NEED IF WE HOST FONTEND AND BACKEND IN THE SAME HOSTING
 // Problem is when after deploying the app on vercel or netlify  I try to navigate the route then it's working but when refresh the page then it's shwoing the page not found
 // When you refresh the page on a route other than the root route ("/"), the server doesn't recognize the route because it's expecting a different server-side route. This results in a "page not found" error
 
-const _dirname = path.resolve();
+// const _dirname = path.resolve();
 
-// Serve static files from the build folder
-app.use(express.static(path.join(_dirname, 'dist')));
+// // Serve static files from the build folder
+// app.use(express.static(path.join(_dirname, 'dist')));
 
-// Define a wildcard route that serves index.html for all routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(_dirname, 'dist', 'index.html'));
-});
+// // Define a wildcard route that serves index.html for all routes
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(_dirname, 'dist', 'index.html'));
+// });
 //THIS IS THE PROBLEM
 
 //Object Data Modeling (ODM) library for MongoDB and Node.js, to disable strict mode for queries.
