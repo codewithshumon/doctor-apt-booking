2a640677bb61457450b3f7bc91696a442a677af3
Shumon Khan <backlink.shumonkhan@gmail.com>
2024-02-20 16:14:44 +0600

    avoiding restrict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
@@ -7,16 +6,14 @@ import 'react-toastify/dist/ReactToastify.css';
 import { AuthContextProvider } from './context/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthContextProvider>
-      <ToastContainer
-        theme="dark"
-        position="top-right"
-        autoClose={1000}
-        closeOnClick
-        pauseOnHover={false}
-      />
-      <App />
-    </AuthContextProvider>
-  </React.StrictMode>
+  <AuthContextProvider>
+    <ToastContainer
+      theme="dark"
+      position="top-right"
+      autoClose={1000}
+      closeOnClick
+      pauseOnHover={false}
+    />
+    <App />
+  </AuthContextProvider>
 );
